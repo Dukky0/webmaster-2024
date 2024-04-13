554a04b2da69ca3f1767d908acf26cfe2a283881
Dukky <alex.jieying.bloom@gmail.com>
2024-04-12 22:09:06 -0400

    Added content
    a

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const sliderImg = document.getElementById("slider-img");
+const sliderTitle = document.getElementById("slider-title");
+const sliderText = document.getElementById("slider-text");
+const sliderBtn = document.getElementById("slider-btn");
+
+const sliderSwitchL = document.getElementById("slider-switch-l");
+const sliderSwitchR = document.getElementById("slider-switch-r");
+
+function sliderSwitch(direction) {
+    switch (direction) {
+        case "left":
+
+            break;
+        case "right":
+
+            break;
+        default:
+            return;
+    }
+}
