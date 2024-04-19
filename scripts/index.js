a718f47b3b200eb851785c71f8c4f6ec0ab20c4f
Dukky <alex.jieying.bloom@gmail.com>
2024-04-19 13:26:07 -0400

    sldiey
    aa

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const sliderImg = document.getElementById("slider-img");
-const sliderTitle = document.getElementById("slider-title");
-const sliderText = document.getElementById("slider-text");
+const sliderTitle = document.getElementById("slider-title-txt");
+const sliderText = document.getElementById("slider-text-txt");
 const sliderBtn = document.getElementById("slider-btn");
 
 const sliderSwitchL = document.getElementById("slider-switch-l");
@@ -8,19 +8,49 @@ const sliderSwitchR = document.getElementById("slider-switch-r");
 
 const initiativesBtn = document.getElementById("initiatives");
 
-initiativesBtn.addEventListener("click", function() {
-    window.location.href = "../initiatives.html";
-})
+import data from "../resources/pages.json" assert {type: "json"};
 
-function sliderSwitch(direction) {
-    switch (direction) {
-        case "left":
+const pages = data.pages;
 
-            break;
-        case "right":
+sliderSwitchL.addEventListener('click', function() {
+    userInput("back");
+    console.log("back");
+});
 
-            break;
-        default:
-            return;
+sliderSwitchR.addEventListener('click', function() {
+    userInput("forward");
+    console.log("forward");
+});
+
+function displayPage(pageIndex) {
+    const page = pages[pageIndex];
+
+    if (page) {
+        console.log(`Page ${page.id}: ${page.title}`);
+
+        sliderTitle.textContent = `${page.title}`;
+        sliderText.textContent = `${page.content}`;
+
+        sliderBtn.href = `${page.link}`
+
+        sliderImg.src = `${page.img}`;
+    } else {
+        console.log('Page not found');
     }
 }
+
+let pageID = 0;
+displayPage(0);
+
+function userInput(input) {
+    if (input === 'forward') {
+        pageID = (pageID + 1) % pages.length;
+        displayPage(pageID);
+    } else if (input === 'back') {
+        pageID = (pageID - 1 + pages.length) % pages.length;
+        displayPage(pageID);
+    } else {
+        console.log('Invalid input. Please enter "forward" or "back".');
+    }
+}
+
