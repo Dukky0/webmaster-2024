868e1f2b5c6b00665d14993ee7922bba94b1a9e8
Dukky <alex.jieying.bloom@gmail.com>
2024-04-28 20:21:00 -0400

    a
    a

--- scripts/index.js
+++ scripts/index.js
@@ -9,9 +9,76 @@ const sliderSwitchR = document.getElementById("slider-switch-r");
 
 const initiativesBtn = document.getElementById("initiatives");
 
-import data from "../resources/pages.json" assert {type: "json"};
+// fetch("https://dukky0.github.io/webmaster-2024/resources/pages.json")
+//     .then((res) => {
+//         if (!res.ok) {
+//             throw new Error
+//             (`HTTP error! Status: ${res.status}`);
+//         }
+//         return res.json();
+//     })
+//     .then((data) => {
+//         const pages = data.pages;
+//     })
+//     .catch((error) => console.error("Unable to fetch data:", error));
 
-const pages = data.pages;
+fetch("../resources/pages.json")
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error
+            (`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) => {
+        const pages = data.pages;
+        console.log(data)
+
+        function userInput(input) {
+            click++;
+
+            if (input === 'forward') {
+                pageID = (pageID + 1) % pages.length;
+                displayPage(pageID);
+            } else if (input === 'back') {
+                pageID = (pageID - 1 + pages.length) % pages.length;
+                displayPage(pageID);
+            } else {
+                console.log('Invalid input. Please enter "forward" or "back".');
+            }
+        }
+
+        let pageID = 0;
+        displayPage(0);
+
+        function displayPage(pageIndex) {
+            const page = pages[pageIndex];
+            console.log(page);
+
+            if (page) {
+                console.log(`Page ${page.id}: ${page.title}`);
+
+                sliderTitle.textContent = `${page.title}`;
+                sliderText.textContent = `${page.content}`;
+                sliderPage.style.transition = "all 0.5s";
+
+                sliderBtn.href = `${page.link}`
+
+                sliderImg.src = `${page.img}`;
+
+                if ((click % 2) === 0) {
+                    sliderPage.style.backgroundColor = "#bca89f";
+                } else {
+                    sliderPage.style.backgroundColor = "#6c9e61";
+                }
+
+                console.log(click);
+            } else {
+                console.log('Page not found');
+            }
+        }
+    })
+    .catch((error) => console.error("Unable to fetch data:", error));
 
 let click = 0;
 
@@ -24,47 +91,3 @@ sliderSwitchR.addEventListener('click', function() {
     userInput("forward");
     console.log("forward");
 });
-
-function displayPage(pageIndex) {
-    const page = pages[pageIndex];
-
-    if (page) {
-        console.log(`Page ${page.id}: ${page.title}`);
-
-        sliderTitle.textContent = `${page.title}`;
-        sliderText.textContent = `${page.content}`;
-        sliderPage.style.transition = "all 0.5s";
-
-        sliderBtn.href = `${page.link}`
-
-        sliderImg.src = `${page.img}`;
-
-        if ((click % 2) === 0) {
-            sliderPage.style.backgroundColor = "#bca89f";
-        } else {
-            sliderPage.style.backgroundColor = "#6c9e61";
-        }
-
-        console.log(click);
-    } else {
-        console.log('Page not found');
-    }
-}
-
-let pageID = 0;
-displayPage(0);
-
-function userInput(input) {
-    click++;
-
-    if (input === 'forward') {
-        pageID = (pageID + 1) % pages.length;
-        displayPage(pageID);
-    } else if (input === 'back') {
-        pageID = (pageID - 1 + pages.length) % pages.length;
-        displayPage(pageID);
-    } else {
-        console.log('Invalid input. Please enter "forward" or "back".');
-    }
-}
-
